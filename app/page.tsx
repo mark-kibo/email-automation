489465d9f30ee4915654700a6daf23a48dc58deb
mark kibocha <kibochamark@gmail.com>
2024-04-09 12:02:56 +0300

    update on layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="my-2">
+<h3>Email Automation Test Drive</h3>
+      </div>
       <EmailClient/>
     </main>
   );
